88f2c2b98e60b856ff4c7993288cd8035e8f9f3a
himadhcodes <mamohamedhimadh@gmail.com>
2026-02-15 02:06:31 +0530

    Commsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import AdminPage from "./Pages/admin";
+import HomePage from "./Pages/home";
+import LoginPage from "./Pages/login";
+
+
+
 function App() {
   
   return (
     <div>
       <Routes>
-        <Route path="/" element={<AdminPage/>}/>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/admin/*" element={<AdminPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
       </Routes>
     </div>
   )
